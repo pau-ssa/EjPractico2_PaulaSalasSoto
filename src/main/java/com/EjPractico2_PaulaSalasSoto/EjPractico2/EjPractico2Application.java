dc83ea58bcf2be0e73e0f48723cb740d0b6b038e
pau-ssa <134456138+pau-ssa@users.noreply.github.com>
2026-08-06 20:15:59 -0600

    Initial commit

---
+++ src/main/java/com/EjPractico2_PaulaSalasSoto/EjPractico2/EjPractico2Application.java
@@ -0,0 +1,13 @@
+package com.EjPractico2_PaulaSalasSoto.EjPractico2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjPractico2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjPractico2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/EjPractico2_PaulaSalasSoto/EjPractico2/EjPractico2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EjPractico2_PaulaSalasSoto.EjPractico2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjPractico2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
